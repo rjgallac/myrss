937cfa73cd273457654155e463e1ae7f67f8c47a
rjgallac <rjgallac@hotmail.com>
2026-06-20 18:28:18 +0100

    add api to auth endpoint

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
     setError("");
 
     try {
-      const response = await fetch("/auth/login", {
+      const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
